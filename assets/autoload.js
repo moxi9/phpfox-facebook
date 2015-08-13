680cef2a0364aa5e5b3e439c34a3f6ceeb9079bb
Raymond Benc <r@phpfox.com>
2015-08-13 12:43:12 +0200

    Automated Dist: 1439462591

--- assets/autoload.js
+++ assets/autoload.js
@@ -3,20 +3,45 @@
  */
 $Ready(function() {
 
+	if (typeof($Cache.friends) == 'object') {
+		$.each($Cache.friends, function(e, obj) {
+			var r = new RegExp("\{'fb':'([0-9]+)'\}", 'gi');
+			obj.user_image.replace(r, function (match) {
+				match = $.parseJSON(match.replace(new RegExp('\'', 'g'), '"'));
+				obj.user_image = '<img src="http://graph.facebook.com/' + match.fb + '/picture?type=large">';
+				return '';
+			});
+		});
+	}
+
 	/**
 	 * Loop thru all profile images with a connection to Facebook
 	 */
-	$('.image_object:not(.fb_built)[data-object="fb"]').each(function() {
-		var t = $(this),
-			i = new Image(),
-			src = 'http://graph.facebook.com/' + t.data('src') + '/picture?type=large';
+	var checkImages = function() {
+		$('.image_object:not(.fb_built)[data-object="fb"], img[src*="{\'fb\':"]:not(.fb_built)').each(function () {
+			var t = $(this),
+				i = new Image();
 
-		t.addClass('fb_built');
-		i.onload = function() {
-			t.attr('src', src);
-		};
-		i.src = src;
-	});
+			if (t.prop('tagName').toLowerCase() == 'img') {
+				var r = new RegExp("\{'fb':'([0-9]+)'\}", 'gi');
+				var reg = t.attr('src').replace(r, function (match) {
+					match = $.parseJSON(match.replace(new RegExp('\'', 'g'), '"'));
+					t.data('src', match.fb);
+					return '';
+				});
+			}
+
+			var src = 'http://graph.facebook.com/' + t.data('src') + '/picture?type=large';
+
+			t.addClass('fb_built');
+			i.onload = function () {
+				t.attr('src', src);
+			};
+			i.src = src;
+		});
+	};
+
+	checkImages();
 
 	// Add the FB login button
 	if (!$('.fb_login_go').length) {
